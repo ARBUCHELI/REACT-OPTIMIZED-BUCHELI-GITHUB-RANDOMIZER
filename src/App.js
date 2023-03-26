c4c5299e7281f79285526f66adf103274535ae8c
ARBUCHELI <radio_seattle@hotmail.com>
2023-03-26 15:53:15 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { GraphPoint } from './GraphPoint';
+import './styles.css';
 
-function App() {
+export const App = () => {
+  const [showExplainer, setShowExplainer] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='container'>
+        <h1 style={{textAlign: "center", paddingTop: "1em", fontFamily: "monospace", fontWeight: "bold", fontSize: "3em", color: "#bd5734 "}}>BUCHELI GITHUB RANDOMIZER</h1>
+        <div className='e3-container' style={{width: "50%", margin: "auto", paddingTop: "2%"}}>
+          <div className='e3-header'>
+            <div>
+              <h2>GitHub randomizer</h2>
+              <p>Create a randomized GitHub contribution graph</p>
+            </div>
+            <button
+              className='e3-button'
+              onClick={() => setShowExplainer(!showExplainer)}
+            >
+              {showExplainer ? 'Hide' : 'Show'} Explainers
+            </button>
+          </div>
+          {showExplainer && (
+            <p className='e3-explainer'>
+              The git contribution graph below is randomized. Only 20% of the
+              contributions are green.
+            </p>
+          )}
+          <div className='e3-contribution-graph'>
+            {[...Array(10000)].map((_, index) => (
+              <GraphPoint key={index} />
+            ))}
+          </div>
+        </div>
+      </div>
+      <footer style={{textAlign: "center", paddingTop: "2em", fontFamily: "monospace", fontWeight: "bold", fontSize: "2em", color: "#bd5734 "}}>Bucheli Web © 2023</footer>
+    </>
   );
-}
+};
 
 export default App;
